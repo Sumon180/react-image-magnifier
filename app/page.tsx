7614c9d15287d901ba6a18ca70e63f67dcc74353
Sumon Hossain <sumonengr.96@gmail.com>
2024-06-30 23:56:22 +0600

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import image from "@/public/cart-image.jpg";
 
 export default function Home() {
   return (
-    <ImageMagnifier
-      src={image}
-      width={1000}
-      height={1000}
-      alt="single/product-image"
-    />
+    <div className="w-96 overflow-hidden">
+      <ImageMagnifier
+        src={image}
+        width={1000}
+        height={1000}
+        alt="single/product-image"
+      />
+    </div>
   );
 }
